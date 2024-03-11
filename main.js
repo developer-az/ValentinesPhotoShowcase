1e29b967fcbcf8178a6b0346b6d58efbf795d944
Anthony Zhou <Az040205@gmail.com>
2024-03-11 01:21:56 -0400

    added hearts

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 // main.js
+function createHeart() {
+    const heart2 = document.createElement('div');
+    heart2.classList.add('heart');
+    heart2.style.left = Math.random() * (window.innerWidth - 20) + 'px';
+    document.getElementById('heart2-container').appendChild(heart2);
+  
+    setTimeout(() => {
+      heart2.remove();
+    }, 5000);
+  }
+  
+  setInterval(createHeart, 500);
+  
+
 
 document.addEventListener('DOMContentLoaded', function () {
     var checkbox = document.getElementById('open');
